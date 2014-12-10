c07b0c7dce0b75e4526728ed0765cc299ebe0219
vivekb88 <vivekb88@vt.edu>
2014-12-10 13:31:34 -0500

    updated the tree classifier predictions

--- codes/DecisionTree.py
+++ codes/DecisionTree.py
@@ -26,6 +26,8 @@ import numpy as np
 from sklearn import tree
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import classification_report
+from sklearn.metrics import f1_score
+import re
 
 
 ''' Conversion background = 0 
@@ -56,14 +58,25 @@ data = tmp[tmp.columns - ignore_idx].values
 labels = tmp[label_idx].values.reshape(len(tmp))
 
 
+opt_criterion = None
+opt_min_split = None
+opt_f1_score = None
 
+min_splits = range(1, 10, 1) + range(10, 100, 10) + range(100, 1000, 100) + range(1000, 10000, 1000)
 
+def extract_f1_score(text):
+    index = text.find('avg')
+    avg_text = text[index:].strip()
+    return float(re.split(r'\s+', avg_text)[5])
+    
+    
 # split into training and test data
 # 80 - 20 split.
 train_data, test_data, train_labels, test_labels = train_test_split(data, labels, test_size=0.2, random_state=42)
+'''
 for crit in ['gini', 'entropy']:
     
-    clf = tree.DecisionTreeClassifier(criterion=crit, min_samples_split=5000)
+    clf = tree.DecisionTreeClassifier(criterion=crit, min_samples_split=1)
 
     print("Results for criterian = ", crit)    
     # This gets the time in ipython shell.
@@ -71,12 +84,44 @@ for crit in ['gini', 'entropy']:
     %time clf.fit(train_data, train_labels)
     print("Modelling time ends\n\n")
 
-    print(" Accuracy = " , clf.score(test_data, test_labels))
-    print(classification_report(test_labels, clf.predict(test_data)))
-
+    #print(" Accuracy = " , clf.score(test_data, test_labels))
+    prediction =  clf.predict(test_data)   
+    print(classification_report(test_labels, prediction))
+    print("extracting f1 score = ", extract_f1_score(classification_report(test_labels, prediction)))
 
     print("\n\nprediction time starts:")
     %time clf.predict(test_data)
     print("prediction time ends:\n\n")
     
     print("Results for criterian = ", crit, "ENDS****") 
+    
+'''
+
+for crit in ['gini']:
+    
+    for min_samples in  min_splits:
+        print("\n\n\nRunning code for criterian = ", crit, " min samples ", min_samples)           
+        clf = tree.DecisionTreeClassifier(criterion=crit, min_samples_split=min_samples)
+
+         
+        # This gets the time in ipython shell.
+        print("Modelling started:")
+        clf.fit(train_data, train_labels)
+        print("Modelling ended:")
+        
+        prediction =  clf.predict(test_data)   
+        print(classification_report(test_labels, prediction))
+        f1 = extract_f1_score(classification_report(test_labels, prediction))        
+        print("extracting f1 score = ", f1)
+    
+        print("code for criterian = ", crit, " min samples ", min_samples, "ends") 
+        if opt_min_split == None:
+            opt_min_split = min_samples
+            opt_criterion = crit
+            opt_f1_score = f1
+        else:
+            if opt_f1_score < f1:
+                opt_f1_score = f1
+                opt_min_split = min_samples
+                opt_criterion = crit
+        
